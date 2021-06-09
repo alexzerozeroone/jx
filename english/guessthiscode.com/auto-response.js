6979d320bfa389b844eb4387793e3e0536486cce
random-development-hub <github.dev.rand@gmail.com>
2021-06-09 22:07:44 +0300

    updated

--- english/guessthiscode.com/auto-response.js
+++
@@ -1,6 +0,0 @@
-// do f=false to stop
-f = true;
-
-setInterval(() => { 
-  if (f) { $(".btn-answers").click(); gistIndex += 1; refreshUI(); }
-}, 200)
--- english/guessthiscode.com/set-points.js
+++
@@ -1,2 +0,0 @@
-points = prompt("what should the points be set to?");
-// this was an easy one
